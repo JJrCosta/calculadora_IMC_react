d5715fa95a9924c41098b4259ff09802ac319ddb
JJrCosta <jrpercosta@gmail.com>
2023-09-24 07:55:47 -0300

    Add calculator  style

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,16 +1,29 @@
+import './calculator.css'
+
 const Calculator = () => {
     
     return (
         <section className="calculadora">
             <form className="formulario">
-                <label>Altura</label>
-                <input type="number" placeholder="Centimetos"/>
-                <label>Peso</label>
-                <input type="number" placeholder=""/>
+                <fieldset>
+                    <label>Altura</label>
+                    <input type="number" placeholder="m"/>
+                </fieldset>
+                <fieldset>
+                    <label>Peso</label>
+                    <input type="number" placeholder="Kg"/>
+                </fieldset>
                 <button type="button">Calcular</button>
+                <div className="resultado">
+                    IMC XPTO
+                </div>
             </form>
-            <div className="resultado">
-
+            <div className="resultado-info">
+                <p className='resultado-valor'>18,5 ou menos</p>
+                <p className='resultado-indice'>Abaixo do normal</p>
+                <p className='resultado-descript'>
+                    Algumas pessoas têm um baixo peso por características do seu organismo e tudo bem. Outras podem estar enfrentando problemas, como a desnutrição. É preciso saber qual é o caso. Procure um médico.
+                </p>
             </div>
         </section>
     )
